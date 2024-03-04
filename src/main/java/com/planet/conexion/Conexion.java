45fb932125a37be3a31ab03a0a358d34e53773ee
AngelVGPM <angel.valdez@planetmedia.com>
2024-03-04 09:31:30 -0600

    sistema de estudiantes v1

---
+++ src/main/java/com/planet/conexion/Conexion.java
@@ -0,0 +1,36 @@
+package com.planet.conexion;
+
+import com.planet.dominio.Estudiante;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Conexion {
+    public static Connection getConexion(){
+        Connection conexion = null;
+        // Nombre del esquema o base de datos
+        var baseDatos = "estudiantes_db";
+        var url = "jdbc:mysql://localhost:3306/" + baseDatos;
+        var usuario = "root";
+        var password = "PM3d1a21@";
+        // Cargamos la clase del driver de mysql en memoria
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection(url, usuario, password);
+        } catch(ClassNotFoundException | SQLException e){
+            System.out.println("Ocurrio un error en la conexion: " + e.getMessage());
+        }
+        return conexion;
+    }
+
+    public static void main(String[] args) {
+        var conexion = Conexion.getConexion();
+        if(conexion != null)
+            System.out.println("Conexion exitosa: " + conexion);
+        else
+            System.out.println("Error al conectarse");
+    }
+
+}
